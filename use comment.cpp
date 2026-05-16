d1c6cf6c5b8a6498e2c632d170adb8d8ebb46fe3
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2026-05-16 18:37:46 +0600

    Add files via upload

---
+++ use comment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	
+	//enter my name 
+	cout<<"Nasrullah"<<endl;
+	/*enter my village name 
+	and my phone number together*/
+	cout<<"Bagan bari"<<endl;
+	cout<<"01518988586";
+	getch();
+}
