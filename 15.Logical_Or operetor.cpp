ae448c57cf78bb0ae7b908d44f4aaa3eacda74f8
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2026-05-18 23:29:42 +0600

    Update 15.Logical_Or operetor.cpp

--- 15.Logical_Or operetor.cpp
+++ 15.Logical_Or operetor.cpp
@@ -6,6 +6,7 @@ int main(){
 	char ch;
 	cout<<"Enter a letter: ";
 	cin>>ch;
+	//here we just take lower case charecter that's why we use "tolower"
 	ch= tolower(ch);
 	if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
 		cout<<"vowel";
