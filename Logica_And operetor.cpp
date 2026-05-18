f5b09db883617c958a529e7cac6d85259f1458d5
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2026-05-18 23:22:48 +0600

    Add files via upload

---
+++ Logica_And operetor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"Enter any two numbers: ";
+	cin>>x>>y;
+	if(x>14&&y<5){
+		cout<<"valid";
+	}
+	else{
+		cout<<"invalid";
+	}
+	
+
+	getch();
+	
+}
+
