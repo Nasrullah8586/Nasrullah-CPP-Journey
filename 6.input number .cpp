0ceb11a6ec7a1505cbbb6b2b77a284cd00444b4d
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2026-05-18 18:53:12 +0600

    Add files via upload

---
+++ 6.input number .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	
+	int num;
+	cout<<"Enter a integer number: ";
+	cin>>num;
+	
+	cout<<"The number is :"<<num;
+	
+	getch();
+	
+}
+
+
