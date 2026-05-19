00bc4842ca28ec952f902d3e366932381f12bd57
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2026-05-19 16:30:27 +0600

    Add files via upload

---
+++ 25.Basic dowhile_loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	
+	
+	int i=1;
+	
+	do{
+		cout<<"Bangladesh: "<<i<<endl;
+		i++;
+	}
+	while(i<=30);
+	
+
+	cout<<"End of while loop";
+	
+
+	getch();
+	
+}
